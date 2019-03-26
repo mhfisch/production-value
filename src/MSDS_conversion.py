836fcd7d473725d09fc1c7c04f66ed21d800415a
mhfisch <maxwellfisch@gmail.com>
2019-03-25 18:00:54 -0700

    add find_producer functionality

--- src/MSDS_conversion.py
+++ src/MSDS_conversion.py
@@ -76,3 +76,82 @@ def list_h5(walk_dir):
     return file_list
 
 
+
+
+
+def find_producer(track, album, artist, year, discogs_token, N=10):
+    """
+    Takes four strings: artist, track, album, and discogs token and returns the tuple (role='Producer', name, and discogs resource_url), 
+    if one is returned in the first N results on discogs.
+    """
+    
+    # Use Discogs API to search for artist, track, album
+    artist = artist.replace(' ','+')
+    track = track.replace(' ','+')
+    album = album.replace(' ','+')
+    api_query = requests.get('https://api.discogs.com/database/search?track={}&artist={}&release_title={}&year={}&type=release&token={}'
+                             .format(track, artist, album, year, discogs_token)).json()['results']
+    
+    #api_query is a LIST. Do the following:
+    
+    producer_list = []
+    #search api_query for 'role' = 'Producer'
+    for i, entry in enumerate(api_query):
+        for producer in gen_key_value_extract('role', entry, 'producer', ['role','name','resource_url']):
+            producer_list.append(producer)
+        
+        #check if we've found a producer yet
+        if len(producer_list) > 0:
+            return producer_list
+        
+        #if no Producer, GET api_subquery from the 'resource_url' field
+        resource_url = list(gen_dict_extract('resource_url', entry))[0]
+        api_subquery = requests.get(resource_url).json()
+            
+        #search next_api_query for 'role' = 'Producer'
+        for producer in gen_key_value_extract('role', api_subquery, 'producer', ['role','name','resource_url']):
+            producer_list.append(producer)  
+            
+        #check if we've found a producer yet
+        if len(producer_list) > 0:
+            return producer_list
+        
+        #If no producer, loop back and go to the next item in api_query.
+        if i>=N:
+            return producer_list
+        
+        
+def gen_key_value_extract(key, var, value, req_keys):
+    """
+    In a nested dictionary, var, where value in key, return req_value for keys in req_keys.
+    """
+    if hasattr(var,'items'):
+        for k, v in var.items():
+            if k == key:
+                if value.lower() in v.lower():
+                    v_list = []
+                    for req_key in req_keys:
+                        v_list.append(var[req_key])
+                    v_tup = tuple(v_list)
+                    yield v_tup
+            if isinstance(v, dict):
+                for result in gen_key_value_extract(key, v, value, req_keys):
+                    yield result
+            elif isinstance(v, list):
+                for d in v:
+                    for result in gen_key_value_extract(key, d, value, req_keys):
+                        yield result
+                        
+
+def gen_dict_extract(key, var):
+    if hasattr(var,'items'):
+        for k, v in var.items():
+            if k == key:
+                yield v
+            if isinstance(v, dict):
+                for result in gen_dict_extract(key, v):
+                    yield result
+            elif isinstance(v, list):
+                for d in v:
+                    for result in gen_dict_extract(key, d):
+                        yield result
